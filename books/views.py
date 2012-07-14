ff57ee6e58e7516d9f021c29f174b221071249a1
vtngsx <vtngsx@gmx.de>
2012-07-14 23:21:05 +0200

    form

--- books/views.py
+++ books/views.py
@@ -1,1 +1,16 @@
 # Create your views here.
+
+from django.shortcuts import render_to_response
+from django.http import HttpResponse
+from books.models import Book
+
+def search(request):
+	error = False
+	if 'q' in request.GET:
+		q = request.GET['q']
+		if q:
+			books = Book.objects.filter(title__icontains=q)
+			return render_to_response('search_results.html', {'books': books, 'query': q})
+		else:
+			error = True
+	return render_to_response('search_form.html', {'error': error})
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from mysite.views import hello, current_datetime, hours_ahead
+from books import views
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -9,6 +10,7 @@ urlpatterns = patterns('',
 	url('^hello/$', hello),
 	url('^time/$', current_datetime),
 	url(r'^time/plus/(\d{1,2})/$', hours_ahead),
+    url(r'^search/$', views.search),
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^mysite/', include('mysite.foo.urls')),

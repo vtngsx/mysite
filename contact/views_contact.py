34efb71628617fccd3052a9255696ac0c08b7445
vtngsx <vtngsx@gmx.de>
2012-07-15 16:28:52 +0200

    form
    contact form angepasst

--- contact/views_contact.py
+++ contact/views_contact.py
@@ -3,27 +3,26 @@
 from django.core.mail import send_mail
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
-from django.template import RequestContext 
+from django.template import RequestContext
+from contact.forms import ContactForm
 
 def contact(request):
 	errors = []
 	if request.method == 'POST':
-		if not request.POST.get('subject', ''):
-			errors.append('Enter a subject.')
-		if not request.POST.get('message', ''):
-			errors.append('Enter a message.')
-		if request.POST.get('e-mail') and '@' not in request.POST['e-mail']:
-			errors.append('Enter a valid e-mail address.')
-		if not errors:
+		form = ContactForm(request.POST)
+		if form.is_valid():
+			cd = form.cleaned_data
 			#send_mail(
-			#	request.POST['subject'],
-			#	request.POST['message'],
-			#	request.POST.get('e-mail', 'noreply@example.com'),
+			#	cd['subject'],
+			#	cd['message'],
+			#	cd.get('e-mail', 'noreply@example.com'),
 			#	['siteowner@example.com'],
 			#)
 			return HttpResponseRedirect('/contact/thanks/')
-	#return render_to_response('contact_form.html', c)
-	return render_to_response('contact_form.html', {'errors' : errors}, context_instance=RequestContext(request))
+	else:
+		#form = ContactForm(initial={'subject': 'I love your site!'})
+		form = ContactForm()
+	return render_to_response('contact_form.html', {'form' : form}, context_instance=RequestContext(request))
 
 def thanks(request):
 	return render_to_response('contact_thanks.html')
